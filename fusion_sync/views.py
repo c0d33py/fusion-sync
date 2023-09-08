9dca045c8e01e93bab0d77a3c1ff490dfc5deb21
c0d33py <rootkill007@gmail.com>
2023-09-08 21:33:50 +0500

    update

--- fusion_sync/views.py
+++ fusion_sync/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import FusionSyncForm
+from .models import FusionSync
+
+
+def index(request):
+    """Index view"""
+    if request.method == 'POST':
+        form = FusionSyncForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    else:
+        form = FusionSyncForm()
+
+    context = {
+        'form': form,
+        'fusions': FusionSync.objects.all(),
+    }
+    return render(request, 'index_forms.html', context)
--- server/settings.py
+++ server/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'minio_storage',
     's3_file_field',
     'fusion_sync',
 ]
